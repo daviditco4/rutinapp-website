9d342dea19ed033576795aa5b1c8a7666e53a9cb
Desiree Limachi <desiree.limachi@gmail.com>
2021-10-09 15:48:33 -0300

    agregado de fondo en home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,21 @@
 <template>
 
   <div class="home">
-    <AppBar />
-    <v-img src="@/assets/fondo1.jpeg" >
+    <!-- <v-img src="@/assets/fondo1.jpeg" > -->
       <v-row>
-        <v-col cols="10" justify-self="center" class="text-center">
+        <v-col offset-sm="1" cols="10" justify-self="center" class="text-center">
           <h1 style="color: white; font-weight:900;">Hola otra vez, Marcela!</h1>
         </v-col>
-        <v-col cols="1">
+        <!-- <v-col cols="1">
           <v-tab to="/miPerfil" >
           <v-img src="@/assets/logo.png" max-width="50" max-height="50" class="justify-center my-3"></v-img>
           </v-tab>
-        </v-col>
+        </v-col> -->
       </v-row>
 
-      <Creaciones v-if="mostrarCreaciones"/>
+
      
-      <v-card v-if="!mostrarCreaciones" width="500" height="370" class="mx-auto mt-16 text-center" color="#76EAAB">
+      <v-card v-if="!mostrarCreaciones" width="500" height="370" class="mx-auto text-center" color="#76EAAB">
         
          <v-card-title primary-title class="text-center" style="color:#333C8E; font-size:24px;">
           ¿Qué tienes ganas de hacer?
@@ -45,22 +44,19 @@
         </v-card-actions> 
       </v-card>
 
-    </v-img>
-  <Footer />
+      <Creaciones v-if="mostrarCreaciones"/>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Footer from '@/components/Footer.vue'
-import AppBar from '@/components/AppBar.vue'
+
 import Creaciones from '@/components/Creaciones.vue'
 
   export default {
     name: 'Home',
     components: {
-      Footer,
-      AppBar,
       Creaciones,
     },
     data: () => ({
@@ -78,10 +74,10 @@ import Creaciones from '@/components/Creaciones.vue'
 </script>
 
 <style lang="scss">
-#textcenter{
-    color:white;
-    width: 250px;
-    height: 250px;
-    line-height: 200px;
+.homeBackground {
+  background: url("../assets/home.jpeg") ;
+  background-size: cover;
+  /*height: 100%;*/
+  opacity: 1;
 }
 </style>

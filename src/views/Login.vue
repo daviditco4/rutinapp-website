523f65ac2e67bba07a2d1f1712328bb5bda7325a
Azul <azkim@itba.edu.ar>
2021-10-09 14:25:58 -0300

    Login and Sign Up views added

---
+++ src/views/Login.vue
@@ -0,0 +1,108 @@
+<template>
+  <v-container fluid>
+<!--    <v-img src="@/assets/login.png">-->
+    <v-spacer/>
+    <v-row justify="center">
+<!--      <v-col cols="12">-->
+          <h1 class="text-center welcome-message">¡Qué alegría volverte a ver!<br>
+            Por favor, iniciá sesión para continuar</h1>
+<!--      </v-col>-->
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col cols="5">
+        <v-sheet
+            color="#76EAAB"
+            rounded
+            elevation="10"
+            class="px-8 pt-4 pa-2 card-format text-center">
+          <v-form>
+            <v-text-field
+                flat
+                outlined
+                light
+                hide-details
+                background-color="white"
+                class="my-2 font-italic"
+                label="Email">
+            </v-text-field>
+            <v-text-field
+                flat
+                outlined
+                light
+                hide-details
+                background-color="white"
+                class="my-2 font-italic"
+                label="Contraseña">
+            </v-text-field>
+            <v-btn
+                class="text-center my-2"
+                color="secondary"
+                elevation="2"
+                raised
+                rounded
+            >Iniciar Sesión
+            </v-btn>
+          </v-form>
+        </v-sheet>
+      </v-col>
+    </v-row>
+<!--    </v-img>-->
+  </v-container>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    const spacers = Array.from({ length: 17 }, (val, i) => `${i}`)
+    // const nspacers = Array.from({ length: 16 }, (val, i) => `n${i + 1}`)
+    const defaults = ['auto', ...spacers]
+
+    return {
+      directions: ['t', 'b', 'l', 'r', 's', 'e', 'x', 'y', 'a'],
+      // marginDirection: 'a',
+      // marginSize: '2',
+      // marginSizes: [...defaults, ...nspacers],
+      paddingDirection: 'a',
+      paddingSize: '6',
+      paddingSizes: defaults,
+      playgroundText: 'Use the controls above to try out the different spacing helpers.',
+    }
+  },
+
+  computed: {
+    computedPadding () {
+      return `p${this.paddingDirection}-${this.paddingSize}`
+    },
+
+  },
+}
+</script>
+
+<style scoped>
+.web-display {
+  display: flex;
+  flex-direction: column;
+}
+.loginBackground {
+  background: url("../assets/login.png") ;
+  background-size: cover;
+  /*height: 100%;*/
+  opacity: 1;
+}
+.welcome-message {
+  color: white;
+  align-content: center;
+  text-shadow: 2px 2px 4px #1C1C1C;
+}
+.user-login-info {
+  border: 2px #333C8E;
+  background-color: #76EAAB;
+  color: #333C8E;
+}
+.user-text {
+  font-size: smaller;
+
+}
+</style>
---
+++ src/views/SignUp.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-img src="@/assets/signup.jpg">
+  <v-container fluid fill-height>
+    <v-spacer/>
+    <v-row class="fill-height" justify="center" align="center">
+      <v-col cols="5">
+        <v-sheet
+            color="#76EAAB"
+            rounded
+            elevation="10"
+            class="px-8 pt-4 pa-2 card-format text-center">
+          <v-form>
+            <v-text-field
+              class="my-2 font-italic"
+              label="Usuario">
+            </v-text-field>
+            <v-text-field
+              class="my-2 font-italic"
+              label="Email">
+            </v-text-field>
+            <v-text-field
+                class="my-2 font-italic"
+                label="Email">
+            </v-text-field>
+            <v-text-field
+                class="my-2 font-italic"
+                label="Contraseña">
+            </v-text-field>
+            <v-text-field
+                class="my-2 font-italic"
+                label="Contraseña">
+            </v-text-field>
+            <v-btn
+                class="text-center my-2"
+                color="secondary"
+                elevation="2"
+                raised
+                rounded
+            >Registrarse
+            </v-btn>
+          </v-form>
+        </v-sheet>
+      </v-col>
+      <v-col cols="7">
+        <div>
+          <h1 class="text-right signup-message">¡Sé parte de la comunidad
+          que entrena donde sea, cuando quiera!</h1>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+  </v-img>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+}
+</script>
+
+<style scoped>
+  .signup-background {
+    background: url("../assets/signup.jpg") no-repeat transparent;
+    background-size: cover;
+    height: 100%;
+    opacity: 1;
+  }
+  .card-format {
+    border: 4px #333C8E;
+  }
+
+  .signup-message {
+    color: white;
+    align-content: center;
+    text-shadow: 2px 2px 4px #1C1C1C;
+  }
+
+  /*.input-bg-color {*/
+  /*  background-color: white;*/
+  /*}*/
+
+</style>

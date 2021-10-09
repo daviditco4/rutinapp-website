8d0c41bde1fa5e5bf11c477275701e424b51c441
Desiree <58440822+dlimachi@users.noreply.github.com>
2021-10-09 11:40:52 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <div id="nav">
+    </div>
     <router-view/>
   </div>
 </template>
@@ -9,8 +11,21 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
 }
 
+#nav {
+  padding: 27px;
 
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
 </style>
+

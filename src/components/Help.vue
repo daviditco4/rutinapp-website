378aa008d75000bd7e2a5ed14f667952b121c9be
Desiree Limachi <desiree.limachi@gmail.com>
2021-10-11 20:07:44 -0300

    cambio en help

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card color="primary" class="mx-auto text-center" width="1100" >
+  <v-card class="mx-auto text-center" width="1100" >
     <v-list>
       <v-list-group :value="true" prepend-icon="mdi-help">
         <template v-slot:activator>
@@ -8,9 +8,12 @@
             <!-- <v-list-item-title >
               <h5> Somos </h5>
             </v-list-item-title> -->
-             <h5 class="text-center"> Somos una pagina donde podran crear rutinas y ejercicios,
-                  podran visualizarlos dentro de la pagina o en la apliacion.
-                </h5>
+             <h5 class="text-center"> 
+               Somos estudiantes del ITBA.
+               Creamos esta pagina donde podran crear/editar/mirar rutinas y ejercicios,
+               y podran visualizarlos dentro de la pagina o en la apliacion.
+
+              </h5>
           </v-list-item>
       </v-list-group>
 
@@ -22,37 +25,74 @@
         <v-list-group :value="true" no-action sub-group >
           <template v-slot:activator>
             <v-list-item-content>
-              <v-list-item-title>Mis Biblioteca</v-list-item-title>
+              <v-list-item-title>Mi Biblioteca</v-list-item-title>              
             </v-list-item-content>
           </template>
 
          <v-list-item >
-             <h5 class="text-center"> Podran visualizar las rutinas creadas por ustedes
+             <h5 class="text-center"> 
+               Podran visualizar las rutinas creadas por el usuario activo y filtrar 
+               su busqueda para un mejor acceso.
                 </h5>
           </v-list-item>
         </v-list-group>
       </v-list-group>
 
 
-      <v-list-group :value="true" prepend-icon="mdi-help">
+      <v-list-group :value="true" prepend-icon="mdi-weight-lifter">
           <template v-slot:activator>
           <v-list-item-title>Crear Rutina</v-list-item-title> </template>
           <v-list-item >
 
               <h5 class="text-center"> 
-                  </h5>
+                Se puede acceder a la creacion de un ejercicio mediante la barra 
+                de tareas en el borde de arriba, o en el inicio donde se vera 
+                "Crear rutina o ejercicio", se piden los datos de:
+                <br/>
+                -Nombre
+                <br/>
+                -Descripcion
+                <br/>
+                -Ciclos
+                </h5>
             </v-list-item>
       </v-list-group>
       
-      <v-list-group :value="true" prepend-icon="mdi-help">
+      <v-list-group :value="true" prepend-icon="mdi-dumbbell">
           <template v-slot:activator>
           <v-list-item-title>Crear Ejercicio</v-list-item-title> </template>
           <v-list-item >
 
-              <h5 class="text-center"> 
-                  </h5>
+              <h5 > 
+                Se puede acceder a la creacion de un ejercicio mediante la barra 
+                de tareas en el borde de arriba, o en el inicio donde se vera 
+                "Crear rutina o ejercicio", se piden los datos de:
+              <br/>
+                -Nombre
+                <br/>
+                -Descripcion
+                <br/>
+                -Series
+                <br/>
+                -Duracion
+                <br/>
+                -Archivos (de forma opcional)
+                <br/>
+                -Categoria
+                
+                </h5>
             </v-list-item>
       </v-list-group>
     </v-list>
   </v-card>
 </template>
+
+<style >
+
+.v-application .v-card{
+  background: '#333C8E';
+}
+.v-application .primary--text {
+    color: black !important;
+}
+</style>

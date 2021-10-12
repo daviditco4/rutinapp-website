3c87f64a14608a57a61ff0c03c96f1cd3c9e6041
Desiree Limachi <desiree.limachi@gmail.com>
2021-10-12 13:59:53 -0300

    esta quedando lindo

--- src/components/modifyCounter.vue
+++ src/components/modifyCounter.vue
@@ -6,7 +6,8 @@
         <!-- <v-card-text class="black--text text-center pl-0 pr-0 py-2 align-center justify-center" style="font-size:20px">
          {{ counter }}
         </v-card-text> -->
-        <v-text-field rounded background-color="white" pl-0 pr-0 py-2 align-center  justify-center :value="counter"> {{ counter }}</v-text-field>
+
+        <v-text-field dense rounded background-color="white" pl-0 pr-0 py-2 align-center  justify-center :value="counter"> {{ counter }}</v-text-field>
       <v-btn class="black--text" style="font-size:25px" icon @click="addTimes()">+</v-btn>
     </v-row>
   </div>

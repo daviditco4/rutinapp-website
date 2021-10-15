f511d2a012d2429b46bb5ecc89ab6a74e495a1a6
dlimachi <desiree.limachi@gmail.com>
2021-10-15 20:04:47 -0300

    centrado

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,26 +6,20 @@
         <v-col offset-sm="1" cols="10" justify-self="center" class="text-center">
           <h1 style="color: white; font-weight:900;">Hola otra vez, {{ username }}!</h1>
         </v-col>
-        <!-- <v-col cols="1">
-          <v-tab to="/miPerfil" >
-          <v-img src="@/assets/logo.png" max-width="50" max-height="50" class="justify-center my-3"></v-img>
-          </v-tab>
-        </v-col> -->
       </v-row>
 
 
      
-      <v-card v-if="!mostrarCreaciones" 
-        width="500" height="370" class="mx-auto text-center" color="#76EAAB">
-        
-         <v-card-title primary-title class="text-center" style="color:#333C8E; font-size:24px;">
-          ¿Qué tienes ganas de hacer?
-        </v-card-title>
+      <v-card v-if="!mostrarCreaciones" width="500" height="370" class="mx-auto text-center" color="#76EAAB">
+
+        <v-row>
+          <v-col md="12"><h3 primary-title class="text-center" color="secondary" style="font-size:24px;">¿Qué tienes ganas de hacer?</h3></v-col>
+        </v-row>
 
         <v-card-actions>
           <v-col>
             <div class="text-center">
-              <v-btn style="background-color: #333C8E; color: white;"
+              <v-btn rounded style="background-color: #333C8E; color: white;"
                x-large block @click="elegir()">Crear Rutina o Ejercicio</v-btn>
             </div>  
           </v-col>
@@ -33,14 +27,14 @@
         <v-card-actions>
           <v-col>    
             <div class="text-center">
-              <v-btn style="background-color: #333C8E; color: white;" x-large block to="/library">Ir a Mi Biblioteca</v-btn>
+              <v-btn rounded style="background-color: #333C8E; color: white;" x-large block to="/library">Ir a Mi Biblioteca</v-btn>
             </div>  
           </v-col> 
         </v-card-actions>
         <v-card-actions>
           <v-col> 
             <div class="text-center">
-              <v-btn style="background-color: #333C8E; color: white;" x-large block to="/search-routine">Buscar Rutinas</v-btn>
+              <v-btn rounded style="background-color: #333C8E; color: white;" x-large block to="/search-routine">Buscar Rutinas</v-btn>
             </div>
           </v-col>  
         </v-card-actions> 

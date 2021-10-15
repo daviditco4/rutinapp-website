8cc301f9645eda61ec3fe860ea396ff328b3376d
dlimachi <desiree.limachi@gmail.com>
2021-10-14 22:03:49 -0300

    borde

--- src/views/CreateExercise.vue
+++ src/views/CreateExercise.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="create-exercise">
     <v-container>
-      <v-card class="pa-md-4 mx-lg-auto mt-16 rounded-xl" width="800px" color="primary">
+      <v-card class="pa-md-4 mx-lg-auto mt-16 " width="800px" color="primary">
         <v-row>
           <v-col>
             <h2 v-if="!$editValue" class="d-flex justify-center">Nuevo ejercicio</h2>
